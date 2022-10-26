0f18ec7596246eefab5f40b88732afe28654c722
19-KaiYang <emersontan1410022@gmail.com>
2022-10-26 11:46:42 +0800

    Restrict player Movement Vertically

---
+++ 15_EmersonTan_Lab08/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private float maxY = 4.87f , minY = 2.93f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        if(transform.position.y < minY)
+        {
+            transform.position = new Vector3(transform.position.x , minY ,0);
+        }
+
+        else if(transform.position.y > minY)
+        {
+            transform.position = new Vector3(transform.position.x , maxY ,0);
+        }
+
+    }
+    public void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
